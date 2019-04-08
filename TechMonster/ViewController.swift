2005b02bf459de799d66b2ded806104dfb7c0d7d
sakine218 <snishibayashi218@gmail.com>
2019-04-08 16:05:43 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by Sakine Nishibayashi on 2019/04/08.
+//  Copyright © 2019 Sakine Nishibayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
